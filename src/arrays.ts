4fa584bd168ce74a7db303a5b89204a9fceb469d
anthony <anittoly@udel.edu>
2025-09-10 10:48:59 -0400

    finished tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    let triple = numbers.map((numbers: number): number => numbers * 3);
+    return triple;
 }
 
 /**
