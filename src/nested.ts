bdab203f96df9907158f545ced552a4a71155528
anthony <anittoly@udel.edu>
2025-09-19 18:41:54 -0400

    completed toCSV function

--- src/nested.ts
+++ src/nested.ts
@@ -124,7 +124,25 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const copy = questions.map(
+        (question: Question): Question => ({ ...question }),
+    );
+    const CSV = copy
+        .map(
+            (question: Question): string =>
+                question.id +
+                "," +
+                question.name +
+                "," +
+                question.options.length +
+                "," +
+                question.points +
+                "," +
+                question.published,
+        )
+        .join("\n");
+
+    return "id,name,options,points,published" + "\n" + CSV;
 }
 
 /**
